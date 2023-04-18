be69304a202edbb98d0ae4387fc10689a15630a3
vmluweb <vina.marieluce@gmail.com>
2023-04-18 11:26:41 +0200

    declaration de variable & factorisation header, footer, home

---
+++ footer.php
@@ -0,0 +1,3 @@
+<p>
+    <strong>© THE ARTBOX</strong> - <em>Tous droits réservés</em>
+</p>
---
+++ header.php
@@ -0,0 +1,6 @@
+<a href="index.html"><img src="img/logo.png" alt="Logo Artbox" id="logo"></a>
+<nav>
+    <ul>
+        <li><a href="index.html">Accueil</a></li>
+    </ul>
+</nav>
